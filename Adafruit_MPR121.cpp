a6eedadb4bd3a8f599fca43571a2ea7e70db266c
Simon Juif <pest23@gmail.com>
2019-04-02 12:36:31 +0200

    Get the angular function from another project,they didn't fork..

--- Adafruit_MPR121.cpp
+++ Adafruit_MPR121.cpp
@@ -1,68 +1,43 @@
-/*!
- * @file Adafruit_MPR121.cpp
- *
- *  @mainpage Adafruit MPR121 arduino driver
- *
- *  @section intro_sec Introduction
- *
- *  This is a library for the MPR121 I2C 12-chan Capacitive Sensor
- *
- *  Designed specifically to work with the MPR121 sensor from Adafruit
- *  ----> https://www.adafruit.com/products/1982
- *
- *  These sensors use I2C to communicate, 2+ pins are required to
- *  interface
- *
- *  Adafruit invests time and resources providing this open source code,
- *  please support Adafruit and open-source hardware by purchasing
- *  products from Adafruit!
- *
- *  @section author Author
- *
- *  Written by Limor Fried/Ladyada for Adafruit Industries.
- *
- *  @section license License
- *
- *  BSD license, all text here must be included in any redistribution.
- */
+/*************************************************** 
+  This is a library for the MPR121 I2C 12-chan Capacitive Sensor
+  Designed specifically to work with the MPR121 sensor from Adafruit
+  ----> https://www.adafruit.com/products/1982
+  These sensors use I2C to communicate, 2+ pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+  
+  Modified by Peter Jansen to include support for capacitive touch wheels. 
+ ****************************************************/
 
 #include "Adafruit_MPR121.h"
 
-/*!
- *  @brief      Default constructor
- */
-Adafruit_MPR121::Adafruit_MPR121() {}
+Adafruit_MPR121::Adafruit_MPR121() {
+}
 
-/*!
- *  @brief    Begin an MPR121 object on a given I2C bus. This function resets
- *            the device and writes the default settings.
- *  @param    i2caddr
- *            the i2c address the device can be found on. Defaults to 0x5A.
- *  @param    *theWire
- *            Wire object
- *  @returns  true on success, false otherwise
- */
-boolean Adafruit_MPR121::begin(uint8_t i2caddr, TwoWire *theWire) {
-
+boolean Adafruit_MPR121::begin(uint8_t i2caddr) {
+  Wire.begin();
+    
   _i2caddr = i2caddr;
-  _wire = theWire;
-
-  _wire->begin();
 
   // soft reset
   writeRegister(MPR121_SOFTRESET, 0x63);
   delay(1);
-  for (uint8_t i = 0; i < 0x7F; i++) {
-    //  Serial.print("$"); Serial.print(i, HEX);
-    //  Serial.print(": 0x"); Serial.println(readRegister8(i));
+  for (uint8_t i=0; i<0x7F; i++) {
+  //  Serial.print("$"); Serial.print(i, HEX); 
+  //  Serial.print(": 0x"); Serial.println(readRegister8(i));
   }
+  
 
-  writeRegister(MPR121_ECR, 0x0);
+  writeRegister(MPR121_ECR, B00001100);//0x0);
 
   uint8_t c = readRegister8(MPR121_CONFIG2);
+  
+  if (c != 0x24) return false;
 
-  if (c != 0x24)
-    return false;
 
   setThreshholds(12, 6);
   writeRegister(MPR121_MHDR, 0x01);
@@ -80,136 +55,216 @@ boolean Adafruit_MPR121::begin(uint8_t i2caddr, TwoWire *theWire) {
   writeRegister(MPR121_FDLT, 0x00);
 
   writeRegister(MPR121_DEBOUNCE, 0);
-  writeRegister(MPR121_CONFIG1, 0x10); // default, 16uA charge current
+  writeRegister(MPR121_CONFIG1, 0x01); // default, 16uA charge current
   writeRegister(MPR121_CONFIG2, 0x20); // 0.5uS encoding, 1ms period
 
-  //  writeRegister(MPR121_AUTOCONFIG0, 0x8F);
+  writeRegister(MPR121_AUTOCONFIG0, 0x8F);
 
-  //  writeRegister(MPR121_UPLIMIT, 150);
-  //  writeRegister(MPR121_TARGETLIMIT, 100); // should be ~400 (100 shifted)
-  //  writeRegister(MPR121_LOWLIMIT, 50);
+  writeRegister(MPR121_UPLIMIT, 150);
+  writeRegister(MPR121_TARGETLIMIT, 100); // should be ~400 (100 shifted)
+  writeRegister(MPR121_LOWLIMIT, 50);
   // enable all electrodes
-  writeRegister(MPR121_ECR,
-                0x8F); // start with first 5 bits of baseline tracking
+  writeRegister(MPR121_ECR, 0x8F);  // start with first 5 bits of baseline tracking 
 
   return true;
 }
 
-/*!
- *  @brief      DEPRECATED. Use Adafruit_MPR121::setThresholds(uint8_t touch,
- *              uint8_t release) instead.
- *  @param      touch
- *              see Adafruit_MPR121::setThresholds(uint8_t touch, uint8_t *release)
- *  @param      release
- *              see Adafruit_MPR121::setThresholds(uint8_t touch, *uint8_t release)
- */
 void Adafruit_MPR121::setThreshholds(uint8_t touch, uint8_t release) {
-  setThresholds(touch, release);
-}
-
-/*!
- *  @brief      Set the touch and release thresholds for all 13 channels on the
- *              device to the passed values. The threshold is defined as a
- *              deviation value from the baseline value, so it remains constant even baseline
- *              value changes. Typically the touch threshold is a little bigger than the
- *              release threshold to touch debounce and hysteresis. For typical touch
- *              application, the value can be in range 0x05~0x30 for example. The setting of
- *              the threshold is depended on the actual application. For the operation details
- *              and how to set the threshold refer to application note AN3892 and MPR121
- *              design guidelines.
- *  @param      touch 
- *              the touch threshold value from 0 to 255.
- *  @param      release 
- *              the release threshold from 0 to 255.
- */
-void Adafruit_MPR121::setThresholds(uint8_t touch, uint8_t release) {
-  for (uint8_t i = 0; i < 12; i++) {
-    writeRegister(MPR121_TOUCHTH_0 + 2 * i, touch);
-    writeRegister(MPR121_RELEASETH_0 + 2 * i, release);
+  for (uint8_t i=0; i<12; i++) {
+    writeRegister(MPR121_TOUCHTH_0 + 2*i, touch);
+    writeRegister(MPR121_RELEASETH_0 + 2*i, release);
   }
 }
 
-/*!
- *  @brief      Read the filtered data from channel t. The ADC raw data outputs
- *              run through 3 levels of digital filtering to filter out the high frequency and
- *              low frequency noise encountered. For detailed information on this filtering
- *              see page 6 of the device datasheet.
- *  @param      t
- *              the channel to read
- *  @returns    the filtered reading as a 10 bit unsigned value
- */
-uint16_t Adafruit_MPR121::filteredData(uint8_t t) {
-  if (t > 12)
-    return 0;
-  return readRegister16(MPR121_FILTDATA_0L + t * 2);
+uint16_t  Adafruit_MPR121::filteredData(uint8_t t) {
+  if (t > 12) return 0;
+  return readRegister16(MPR121_FILTDATA_0L + t*2);
 }
 
-/*!
- *  @brief      Read the baseline value for the channel. The 3rd level filtered
- *              result is internally 10bit but only high 8 bits are readable from registers
- *              0x1E~0x2A as the baseline value output for each channel.
- *  @param      t 
- *              the channel to read.
- *  @returns    the baseline data that was read
- */
-uint16_t Adafruit_MPR121::baselineData(uint8_t t) {
-  if (t > 12)
-    return 0;
+uint16_t  Adafruit_MPR121::baselineData(uint8_t t) {
+  if (t > 12) return 0;
   uint16_t bl = readRegister8(MPR121_BASELINE_0 + t);
   return (bl << 2);
 }
 
-/**
- *  @brief      Read the touch status of all 13 channels as bit values in a 12 bit integer.
- *  @returns    a 12 bit integer with each bit corresponding to the touch status
- *              of a sensor. For example, if bit 0 is set then channel 0 of the device is
- *              currently deemed to be touched.
- */
-uint16_t Adafruit_MPR121::touched(void) {
+uint16_t  Adafruit_MPR121::touched(void) {
   uint16_t t = readRegister16(MPR121_TOUCHSTATUS_L);
+
   return t & 0x0FFF;
 }
 
-/*!
- *  @brief      Read the contents of an 8 bit device register.
- *  @param      reg the register address to read from
- *  @returns    the 8 bit value that was read.
- */
+/*********************************************************************/
+// Touch Wheel
+void Adafruit_MPR121::initTouchWheel(uint8_t numWheelPads, int16_t degreeOffset) {
+  wheelPads = numWheelPads;
+  wheelDegreeOffset = degreeOffset;
+  lastWheelAngle = -1;
+}
+
+void Adafruit_MPR121::takeWheelBaseline(void) {
+  for (uint8_t i=0; i<12; i++) {
+    wheelBaseline[i] = filteredData(i);
+  }   
+}
+
+int16_t Adafruit_MPR121::getWheelAngle(void) {
+  // Retrieve difference scores
+  int16_t wheelDeltas[8];
+  for (uint8_t i=0; i<wheelPads; i++) {
+    wheelDeltas[i] = wheelBaseline[i] - filteredData(i);
+  } 
+
+
+  // Display deltas (debug)
+  
+  Serial.print ("baselines: \t");
+  for (uint8_t i=0; i<13; i++) {
+    Serial.print(filteredData(i), DEC);
+    Serial.print("\t");
+  } 
+  Serial.println ("");
+  
+  Serial.print ("deltas: \t");
+  for (uint8_t i=0; i<wheelPads; i++) {
+    Serial.print(wheelDeltas[i], DEC);
+    Serial.print("\t");
+  } 
+  Serial.println ("");
+
+
+  // Find maximum delta
+  int16_t maxVal = 0;  
+  int16_t maxIdx = -1;
+  for (uint8_t i=0; i<wheelPads; i++) {
+     if (wheelDeltas[i] > maxVal) {
+       maxVal = wheelDeltas[i];
+       maxIdx = i;
+     }
+  } 
+  
+  // If there are no significant deltas (ie no strong touch signals), return -1 (no touch wheel activity)
+  if (maxVal < 3) {    // was <20
+    return -1;
+  }
+
+  // Find neighbouring values
+  int16_t v1Idx = (maxIdx-1);
+  if (v1Idx < 0) v1Idx += wheelPads;      // Handle the negitive modulo
+    
+  int16_t v1 = wheelDeltas[v1Idx];                   // Pad before
+  int16_t v2 = wheelDeltas[maxIdx];                  // Pad with highest value
+  int16_t v3 = wheelDeltas[(maxIdx+1) % wheelPads];  // Pad after
+  float sum = v1 + v2 + v3;
+  
+  // Proportions
+  float c1 = v1 / sum;
+  float c2 = v2 / sum;
+  float c3 = v3 / sum;
+  
+  // Offset
+//  float offset = c2 + c3;                // Offset (-0.5 .. +0.5)
+  float offset = 0;
+  if (c1 > c3) {
+    offset = -c1;
+  } else {
+    offset = c3;
+  }
+  float offsetPad = (float)(maxIdx) + offset;    // Offset relative to pad (e.g. for a 8 pad wheel, 0-7, representing 0-360 degrees)
+  if (offsetPad < 0) offsetPad += wheelPads;     // Bound check incase we go negitive for half of the first wheel
+  
+  // Calculate the touch position (in degrees)
+  int16_t degree = floor( (offsetPad / wheelPads) * 360.0f ) - wheelDegreeOffset;
+  if (degree < 0) degree += 360;
+  if (degree > 360) degree -= 360;
+    
+  return degree;
+}
+
+
+int16_t Adafruit_MPR121::getWheelIncrement(void) {
+  int16_t curWheelAngle = getWheelAngle();
+  int16_t wheelAnglePerTick = 90;
+  
+  // Case 1: Wheel is not being touched)
+  if (curWheelAngle == -1) {
+    lastWheelAngle = curWheelAngle;
+    return 0;
+  }
+  
+  // Case 2: Wheel has just started being touched
+  if ((lastWheelAngle == -1) && (curWheelAngle > -1)) {
+    lastWheelAngle = curWheelAngle;
+    return 0;
+  }
+  
+  // Case 3: Wheel was touched previously, and is currently being touched
+  // Special case: if current wheel angle and previous angle differ by more than 180, then a large move was made, or we're straddling 
+  // the 359/0 boundary. Let's make the math easier.
+  int16_t curWheelAngleAdj = curWheelAngle;
+  if (curWheelAngleAdj < (lastWheelAngle - 180)) {
+    curWheelAngleAdj += 360;
+  } else if (curWheelAngleAdj > (lastWheelAngle + 180)) {
+    curWheelAngleAdj -= 360;
+  }
+
+  Serial.print ("   lastWheelAngle: ");  Serial.println(lastWheelAngle, DEC);
+  Serial.print ("   curWheelAngleAdj: ");  Serial.println(curWheelAngleAdj, DEC);  
+  int16_t delta = (curWheelAngleAdj - lastWheelAngle) / wheelAnglePerTick;
+  Serial.print ("   delta: ");  Serial.println(delta, DEC);  
+  
+  if (delta != 0) {
+    // If we've made a movement that counts for at least one tick, then update lastWheelAngle to reflect the new position of the pointer
+    lastWheelAngle = curWheelAngle;    
+  }
+  
+  
+  // Return the number of inrement/decrement moves we've made  (or 0 if we haven't made any)
+  return delta;
+  
+}
+
+/*********************************************************************/
+// Push buttons
+// For momentary pushbuttons, wired as open/momentarily closed to ground
+/*********************************************************************/
+boolean Adafruit_MPR121::isButtonPressed(uint8_t channel) {
+  if (filteredData(channel) < MPR121_BUTTON_PRESS_THRESH) {
+    return true;
+  } 
+  return false;
+}
+
+
+/*********************************************************************/
+// Low Level Communications
+/*********************************************************************/
+
 uint8_t Adafruit_MPR121::readRegister8(uint8_t reg) {
-  _wire->beginTransmission(_i2caddr);
-  _wire->write(reg);
-  _wire->endTransmission(false);
-  _wire->requestFrom(_i2caddr, 1);
-  if (_wire->available() < 1)
-    return 0;
-  return (_wire->read());
+    Wire.beginTransmission(_i2caddr);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+    Wire.requestFrom(_i2caddr, 1);
+    return ( Wire.read());
 }
 
-/*!
- *  @brief      Read the contents of a 16 bit device register.
- *  @param      reg the register address to read from
- *  @returns    the 16 bit value that was read.
- */
 uint16_t Adafruit_MPR121::readRegister16(uint8_t reg) {
-  _wire->beginTransmission(_i2caddr);
-  _wire->write(reg);
-  _wire->endTransmission(false);
-  _wire->requestFrom(_i2caddr, 2);
-  if (_wire->available() < 2)
-    return 0;
-  uint16_t v = _wire->read();
-  v |= ((uint16_t)_wire->read()) << 8;
-  return v;
+    Wire.beginTransmission(_i2caddr);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+    Wire.requestFrom(_i2caddr, 2);
+    uint16_t v = Wire.read();
+    v |=  ((uint16_t) Wire.read()) << 8;
+    return v;
 }
 
+/**************************************************************************/
 /*!
     @brief  Writes 8-bits to the specified destination register
-    @param  reg the register address to write to
-    @param  value the value to write
 */
+/**************************************************************************/
 void Adafruit_MPR121::writeRegister(uint8_t reg, uint8_t value) {
-  _wire->beginTransmission(_i2caddr);
-  _wire->write((uint8_t)reg);
-  _wire->write((uint8_t)(value));
-  _wire->endTransmission();
+    Wire.beginTransmission(_i2caddr);
+    Wire.write((uint8_t)reg);
+    Wire.write((uint8_t)(value));
+    Wire.endTransmission(true);
 }
--- Adafruit_MPR121.h
+++ Adafruit_MPR121.h
@@ -1,105 +1,97 @@
-/*!
- *  @file Adafruit_MPR121.h
- *
- *  This is a library for the MPR121 12-Channel Capacitive Sensor
- *
- *  Designed specifically to work with the MPR121 board.
- *
- *  Pick one up today in the adafruit shop!
- *  ------> https://www.adafruit.com/product/1982
- *
- *  These sensors use I2C to communicate, 2+ pins are required to interface
- *
- *  Adafruit invests time and resources providing this open source code,
- *  please support Adafruit andopen-source hardware by purchasing products
- *  from Adafruit!
- *
- *  Limor Fried/Ladyada (Adafruit Industries).
- *
- *  BSD license, all text above must be included in any redistribution
- */
+/*************************************************** 
+  This is a library for the MPR121 12-Channel Capacitive Sensor
+  Designed specifically to work with the MPR121 breakout from Adafruit
+  ----> https://www.adafruit.com/products/1982
+  These sensors use I2C to communicate, 2+ pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 
-#ifndef ADAFRUIT_MPR121_H
-#define ADAFRUIT_MPR121_H
-
-#include "Arduino.h"
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
 #include <Wire.h>
 
 // The default I2C address
-#define MPR121_I2CADDR_DEFAULT 0x5A ///< default I2C address
+#define MPR121_I2CADDR_DEFAULT 0x5A
 
-/*!
- *  Device register map
- */
-enum {
-  MPR121_TOUCHSTATUS_L = 0x00,
-  MPR121_TOUCHSTATUS_H = 0x01,
-  MPR121_FILTDATA_0L = 0x04,
-  MPR121_FILTDATA_0H = 0x05,
-  MPR121_BASELINE_0 = 0x1E,
-  MPR121_MHDR = 0x2B,
-  MPR121_NHDR = 0x2C,
-  MPR121_NCLR = 0x2D,
-  MPR121_FDLR = 0x2E,
-  MPR121_MHDF = 0x2F,
-  MPR121_NHDF = 0x30,
-  MPR121_NCLF = 0x31,
-  MPR121_FDLF = 0x32,
-  MPR121_NHDT = 0x33,
-  MPR121_NCLT = 0x34,
-  MPR121_FDLT = 0x35,
+#define MPR121_TOUCHSTATUS_L 0x00
+#define MPR121_TOUCHSTATUS_H 0x01
+#define MPR121_FILTDATA_0L  0x04
+#define MPR121_FILTDATA_0H  0x05
+#define MPR121_BASELINE_0   0x1E
+#define MPR121_MHDR         0x2B
+#define MPR121_NHDR         0x2C
+#define MPR121_NCLR         0x2D
+#define MPR121_FDLR         0x2E
+#define MPR121_MHDF         0x2F
+#define MPR121_NHDF         0x30
+#define MPR121_NCLF         0x31
+#define MPR121_FDLF         0x32
+#define MPR121_NHDT         0x33
+#define MPR121_NCLT         0x34
+#define MPR121_FDLT         0x35
 
-  MPR121_TOUCHTH_0 = 0x41,
-  MPR121_RELEASETH_0 = 0x42,
-  MPR121_DEBOUNCE = 0x5B,
-  MPR121_CONFIG1 = 0x5C,
-  MPR121_CONFIG2 = 0x5D,
-  MPR121_CHARGECURR_0 = 0x5F,
-  MPR121_CHARGETIME_1 = 0x6C,
-  MPR121_ECR = 0x5E,
-  MPR121_AUTOCONFIG0 = 0x7B,
-  MPR121_AUTOCONFIG1 = 0x7C,
-  MPR121_UPLIMIT = 0x7D,
-  MPR121_LOWLIMIT = 0x7E,
-  MPR121_TARGETLIMIT = 0x7F,
+#define MPR121_TOUCHTH_0    0x41
+#define MPR121_RELEASETH_0    0x42
+#define MPR121_DEBOUNCE 0x5B
+#define MPR121_CONFIG1 0x5C
+#define MPR121_CONFIG2 0x5D
+#define MPR121_CHARGECURR_0 0x5F
+#define MPR121_CHARGETIME_1 0x6C
+#define MPR121_ECR 0x5E
+#define MPR121_AUTOCONFIG0 0x7B
+#define MPR121_AUTOCONFIG1 0x7C
+#define MPR121_UPLIMIT   0x7D
+#define MPR121_LOWLIMIT  0x7E
+#define MPR121_TARGETLIMIT  0x7F
 
-  MPR121_GPIODIR = 0x76,
-  MPR121_GPIOEN = 0x77,
-  MPR121_GPIOSET = 0x78,
-  MPR121_GPIOCLR = 0x79,
-  MPR121_GPIOTOGGLE = 0x7A,
+#define MPR121_GPIODIR  0x76
+#define MPR121_GPIOEN  0x77
+#define MPR121_GPIOSET  0x78
+#define MPR121_GPIOCLR  0x79
+#define MPR121_GPIOTOGGLE  0x7A
 
-  MPR121_SOFTRESET = 0x80,
-};
+#define MPR121_SOFTRESET 0x80
+
+#define MPR121_BUTTON_PRESS_THRESH  10
 
 //.. thru to 0x1C/0x1D
-
-/*!
- *  @brief  Class that stores state and functions for interacting with MPR121
- *  proximity capacitive touch sensor controller.
- */
 class Adafruit_MPR121 {
-public:
+ public:
+  // Variables
+  int16_t wheelBaseline[12];
+  uint8_t wheelPads;
+  int16_t wheelDegreeOffset;
+  int16_t lastWheelAngle;
+  
   // Hardware I2C
-  Adafruit_MPR121();
+  Adafruit_MPR121(void);
 
-  boolean begin(uint8_t i2caddr = MPR121_I2CADDR_DEFAULT, TwoWire *theWire = &Wire);
+  boolean begin(uint8_t i2caddr = MPR121_I2CADDR_DEFAULT);
 
   uint16_t filteredData(uint8_t t);
-  uint16_t baselineData(uint8_t t);
+  uint16_t  baselineData(uint8_t t);
+
+  void initTouchWheel(uint8_t numWheelPads, int16_t degreeOffset);
+  void takeWheelBaseline(void);
+  int16_t getWheelAngle(void);
+  int16_t getWheelIncrement(void);
+
+  boolean isButtonPressed(uint8_t channel);
 
   uint8_t readRegister8(uint8_t reg);
   uint16_t readRegister16(uint8_t reg);
   void writeRegister(uint8_t reg, uint8_t value);
   uint16_t touched(void);
-  // Add deprecated attribute so that the compiler shows a warning
-  void setThreshholds(uint8_t touch, uint8_t release)
-      __attribute__((deprecated));
-  void setThresholds(uint8_t touch, uint8_t release);
+  void setThreshholds(uint8_t touch, uint8_t release);
 
-private:
+ private:
   int8_t _i2caddr;
-  TwoWire *_wire;
 };
-
-#endif
